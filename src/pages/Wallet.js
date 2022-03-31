8f114748320f2f466fdffb10f887c8b4d3411648
Maju Domenciano <juliadomenciano@hotmail.com>
2022-03-31 19:32:25 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,37 +11,94 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email } = this.props;
+    const { email, currencies } = this.props;
     return (
-      <header>
-        <div>
-          <p>TRYBE WALLET LOGIN</p>
-        </div>
-        <div>
-          <p
-            data-testid="email-field"
-          >
-            Email:
-            {' '}
-            { email }
-          </p>
-          <p
-            data-testid="total-field"
-          >
-            Despesa Total:
-            {' '}
-            { 0 }
-          </p>
-          <p
-            data-testid="header-currency-field"
-          >
-            Despesa Total:
-            {' '}
-            { 'BRL' }
-          </p>
-        </div>
-
-      </header>
+      <>
+        <header>
+          <div>
+            <p>TRYBE WALLET LOGIN</p>
+          </div>
+          <div>
+            <p
+              data-testid="email-field"
+            >
+              { `Email: ${email}` }
+            </p>
+            <p
+              data-testid="total-field"
+            >
+              { `Despesa Total:${0}` }
+            </p>
+            <p
+              data-testid="header-currency-field"
+            >
+              BRL
+            </p>
+          </div>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              type="number"
+              data-testid="value-input"
+              name="value"
+              /*               onChange={} */
+            />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input
+              type="text"
+              data-testid="description-input"
+              name="description"
+              /*               onChange={} */
+            />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select
+              name="currency"
+              id="currency"
+              /*             onChange={} */
+            >
+              {
+                currencies.map((item) => (
+                  <option
+                    key={ item }
+                    value={ item }
+                  >
+                    {item}
+                  </option>))
+              }
+            </select>
+          </label>
+          <label htmlFor="paymentMeyhod">
+            Método de pagamento:
+            <select
+              name="paymentMeyhod"
+              data-testid="method-input"
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="categories">
+            Categorias:
+            <select
+              name="categories"
+              data-testid="tag-input"
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
 
     );
   }
@@ -49,7 +106,7 @@ class Wallet extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
-  /*   currencies: state.wallet.currencies, */
+  currencies: state.wallet.currencies,
 
 });
 
@@ -60,10 +117,12 @@ const mapDispatchToProps = (dispatch) => ({
 Wallet.propTypes = {
   email: PropTypes.string,
   getCurrencies: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string),
 };
 
 Wallet.defaultProps = {
   email: '',
+  currencies: [],
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
